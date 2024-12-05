f14a4ccc9a2200ebc8aee33c1f7656f4e5412152
Nathan Madrid <devsamuelescobar913@gmail.com>
2024-12-04 21:53:54 -0600

    Divs will stay colored if you hover over them

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ for(let i = 0; i < 256; i++){
     document.getElementById("divs-container").appendChild(newDiv);
 }
 
+// when you hover over a div it gets completely colored
+const divs = document.querySelectorAll('.item');
+divs.forEach(div => {
+    div.addEventListener('mouseenter',hoverDiv)
+});
+
+function hoverDiv(event){
+    if (!event.target.classList.contains("colored")) { // Check if the div doesn't have the 'colored' class
+        event.target.classList.add("colored"); // Add the 'colored' class if it doesn't have it
+    }
+}
